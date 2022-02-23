960c8f7f68cce3eb21a6e7fd6002ac62e250e635
Johan Mabille <johan.mabille@gmail.com>
2022-02-23 10:59:59 +0100

    Better error handling

--- src/main.cpp
+++ src/main.cpp
@@ -90,55 +90,72 @@ int main(int argc, char* argv[])
     using history_manager_ptr = std::unique_ptr<xeus::xhistory_manager>;
     history_manager_ptr hist = xeus::make_in_memory_history_manager();
 
-    if (!file_name.empty())
+    try
     {
-        xeus::xconfiguration config = xeus::load_configuration(file_name);
+        if (!file_name.empty())
+        {
+            xeus::xconfiguration config = xeus::load_configuration(file_name);
 
-        xeus::xkernel kernel(config,
-                             xeus::get_user_name(),
-                             std::move(context),
-                             std::move(interpreter),
-                             xeus::make_xserver_shell_main,
-                             std::move(hist),
-                             xeus::make_console_logger(xeus::xlogger::msg_type,
-                                                       xeus::make_file_logger(xeus::xlogger::content, "xeus.log")));
+            xeus::xkernel kernel(config,
+                                 xeus::get_user_name(),
+                                 std::move(context),
+                                 std::move(interpreter),
+                                 xeus::make_xserver_shell_main,
+                                 std::move(hist),
+                                 xeus::make_console_logger(xeus::xlogger::msg_type,
+                                                           xeus::make_file_logger(xeus::xlogger::content, "xeus.log")));
 
-        std::clog <<
-            "Starting xeus-sql kernel...\n\n"
-            "If you want to connect to this kernel from an other client, you can use"
-            " the " + file_name + " file."
-            << std::endl;
+            std::clog <<
+                "Starting xeus-sql kernel...\n\n"
+                "If you want to connect to this kernel from an other client, you can use"
+                " the " + file_name + " file."
+                << std::endl;
 
-        kernel.start();
+            kernel.start();
+        }
+        else
+        {
+            xeus::xkernel kernel(xeus::get_user_name(),
+                                 std::move(context),
+                                 std::move(interpreter),
+                                 xeus::make_xserver_shell_main,
+                                 std::move(hist),
+                                 nullptr);
+
+            const auto& config = kernel.get_config();
+            std::clog <<
+                "Starting xeus-sql kernel...\n\n"
+                "If you want to connect to this kernel from an other client, just copy"
+                " and paste the following content inside of a `kernel.json` file. And then run for example:\n\n"
+                "# jupyter console --existing kernel.json\n\n"
+                "kernel.json\n```\n{\n"
+                "    \"transport\": \"" + config.m_transport + "\",\n"
+                "    \"ip\": \"" + config.m_ip + "\",\n"
+                "    \"control_port\": " + config.m_control_port + ",\n"
+                "    \"shell_port\": " + config.m_shell_port + ",\n"
+                "    \"stdin_port\": " + config.m_stdin_port + ",\n"
+                "    \"iopub_port\": " + config.m_iopub_port + ",\n"
+                "    \"hb_port\": " + config.m_hb_port + ",\n"
+                "    \"signature_scheme\": \"" + config.m_signature_scheme + "\",\n"
+                "    \"key\": \"" + config.m_key + "\"\n"
+                "}\n```\n";
+
+            kernel.start();
+        }
     }
-    else
+    catch(std::exception& e)
     {
-        xeus::xkernel kernel(xeus::get_user_name(),
-                             std::move(context),
-                             std::move(interpreter),
-                             xeus::make_xserver_shell_main,
-                             std::move(hist),
-                             nullptr);
-
-        const auto& config = kernel.get_config();
-        std::clog <<
-            "Starting xeus-sql kernel...\n\n"
-            "If you want to connect to this kernel from an other client, just copy"
-            " and paste the following content inside of a `kernel.json` file. And then run for example:\n\n"
-            "# jupyter console --existing kernel.json\n\n"
-            "kernel.json\n```\n{\n"
-            "    \"transport\": \"" + config.m_transport + "\",\n"
-            "    \"ip\": \"" + config.m_ip + "\",\n"
-            "    \"control_port\": " + config.m_control_port + ",\n"
-            "    \"shell_port\": " + config.m_shell_port + ",\n"
-            "    \"stdin_port\": " + config.m_stdin_port + ",\n"
-            "    \"iopub_port\": " + config.m_iopub_port + ",\n"
-            "    \"hb_port\": " + config.m_hb_port + ",\n"
-            "    \"signature_scheme\": \"" + config.m_signature_scheme + "\",\n"
-            "    \"key\": \"" + config.m_key + "\"\n"
-            "}\n```\n";
-
-        kernel.start();
+        std::clog << e.what() << std::endl;
+        // This is not clean, but we need an additional API in xeus to
+        // stop the kernel in a clean way
+        throw e;
+    }
+    catch(const char* p)
+    {
+        std::clog << p << std::endl;
+        // This is not clean, but we need an additional API in xeus to
+        // stop the kernel in a clean way
+        throw p;
     }
 
     return 0;
